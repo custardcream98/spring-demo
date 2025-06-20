c3b230de539c0063ab2fc06f93d333469602ec4d
shiwoo.park <custardcream@kakao.com>
2025-06-20 17:19:24 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Demo
+
+`motion` Spring Value를 테스트 해볼 수 있는 데모
---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+
+      'perfectionist/sort-imports': 'error',
+      'perfectionist/sort-exports': 'error',
+      'perfectionist/sort-array-includes': 'error',
+      'perfectionist/sort-intersection-types': 'error',
+      'perfectionist/sort-jsx-props': 'error',
+      'perfectionist/sort-interfaces': 'error',
+      'perfectionist/sort-named-exports': 'error',
+      'perfectionist/sort-object-types': 'error',
+      'perfectionist/sort-objects': 'error',
+      'perfectionist/sort-union-types': 'error',
+    },
+    settings: {
+      perfectionist: {
+        type: 'natural',
+      },
+    },
+  }
+)
